a4394a2371df430ca9fd7c83a482fa52d3d61ed5
Michael <mvasilyev04@gmail.com>
2025-03-26 02:58:18 +0300

    implement set bit

--- main.c
+++ main.c
@@ -1,13 +1,21 @@
 #include "bit_vector.h"
 
+static void print_vector(struct Vector* v);
+
 int main() {
   size_t size = 0;
+  printf("Enter size for vector: ");
   scanf("%ld", &size);
   struct Vector* v = init(size);
   if (v == NULL) {
     fprintf(stderr, "Sorry, vector is still NULL\n");
     return EXIT_FAILURE;
   }
+  uint32_t index = 0;
+  printf("Enter index you want set bit: ");
+  scanf("%u", &index);
+  set_bit(v, index);
+  print_vector(v);
   delete_vector(v);
   return EXIT_SUCCESS;
 }
@@ -18,7 +26,7 @@ struct Vector* init(size_t size) {
     fprintf(stderr, "Cannot allocate memory for bit_vector: %d", errno);
     return NULL;
   }
-  bit_vector->array = (uint32_t*)malloc(sizeof(uint32_t) * size);
+  bit_vector->array = (uint32_t*)calloc(size, sizeof(uint32_t));
   if (!bit_vector->array) {
     fprintf(stderr, "Cannot allocate memory for array in bit_vector: %d",
             errno);
@@ -34,4 +42,16 @@ void delete_vector(struct Vector* v) {
     free(v->array);
   }
   free(v);
+}
+
+void set_bit(struct Vector* vector, uint32_t index) {
+  uint32_t array_index = index / 32;
+  uint32_t bit = index % 32;
+  vector->array[array_index] |= 1 << bit;
+}
+
+static void print_vector(struct Vector* v) {
+  for (size_t i = 0; i < v->size; i++) {
+    printf("%u ", v->array[i]);
+  }
 }
