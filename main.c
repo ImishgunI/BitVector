58e5a38b8546966c5c5d9db2b0070ec38a02e896
Michael <mvasilyev04@gmail.com>
2025-03-25 20:12:55 +0300

    added allocation for array in init function

--- main.c
+++ main.c
@@ -1,24 +1,37 @@
-#include <stdlib.h>
-
 #include "bit_vector.h"
 
 int main() {
-  struct Vector* v = init();
+  size_t size = 0;
+  scanf("%ld", &size);
+  struct Vector* v = init(size);
   if (v == NULL) {
     fprintf(stderr, "Sorry, vector is still NULL\n");
     return EXIT_FAILURE;
   }
   delete_vector(v);
-  return 0;
+  return EXIT_SUCCESS;
 }
 
-struct Vector* init() {
+struct Vector* init(size_t size) {
   struct Vector* bit_vector = (struct Vector*)malloc(sizeof(struct Vector));
-  if (bit_vector == NULL) {
+  if (!bit_vector) {
     fprintf(stderr, "Cannot allocate memory for bit_vector: %d", errno);
     return NULL;
   }
+  bit_vector->array = (uint32_t*)malloc(sizeof(uint32_t) * size);
+  if (!bit_vector->array) {
+    fprintf(stderr, "Cannot allocate memory for array in bit_vector: %d",
+            errno);
+    delete_vector(bit_vector);
+    return NULL;
+  }
+  bit_vector->size = size;
   return bit_vector;
 }
 
-void delete_vector(struct Vector* v) { return free(v); }
+void delete_vector(struct Vector* v) {
+  if (v->array) {
+    free(v->array);
+  }
+  free(v);
+}
