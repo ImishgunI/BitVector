e9f9cd0f7795e8cdd8bdfce842a25a7d4aeea987
Michael <mvasilyev04@gmail.com>
2025-04-12 15:34:24 +0300

    added func toggle_bit

--- bit_vector.h
+++ bit_vector.h
@@ -6,6 +6,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct Vector {
   uint32_t* array;
@@ -15,4 +16,6 @@ struct Vector {
 struct Vector* init(size_t size);
 void delete_vector(struct Vector* v);
 void set_bit(struct Vector* vector, uint32_t index);
+void clear_bit(struct Vector* vector, uint32_t index);
+void toggle_bit(struct Vector* vector, uint32_t index);
 #endif  // !BIT_VECTOR
